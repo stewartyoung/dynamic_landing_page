34e7426ed16f1c465378795025809d7ff03eaaaf
Stewart Young <stewart@dresser.app>
2019-06-11 10:19:18 +0100

    Added function to set focus+name in local storage

--- js/main.js
+++ js/main.js
@@ -4,6 +4,9 @@ const time = document.getElementById("time"),
   name = document.getElementById("name"),
   focus = document.getElementById("focus");
 
+// Options
+const showAmPm = true;
+
 // Show time
 function showTime() {
   let today = new Date(),
@@ -20,7 +23,7 @@ function showTime() {
   // output time
   time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(
     sec
-  )}`;
+  )} ${showAmPm ? amPm : ""}`;
 
   setTimeout(showTime, 1000);
 }
@@ -57,6 +60,59 @@ function setBgGreet() {
   document.body.style.backgroundSize = "cover";
 }
 
-// Run (they're asynchronous)
+// Get name function
+function getName() {
+  if (localStorage.getItem("name") === null) {
+    name.textContent = "[Enter Name]";
+  } else {
+    name.textContent = localStorage.getItem("name");
+  }
+}
+
+// Set name function
+function setName(e) {
+  if (e.type === "keypress") {
+    // Enter click handler
+    if (e.which == 13 || e.keyCode == 13) {
+      localStorage.setItem("name", e.target.innerText);
+      name.blur();
+    }
+  } else {
+    // Blur event handler
+    localStorage.setItem("name", e.target.innerText);
+  }
+}
+
+// Set focus function
+function setFocus(e) {
+  if (e.type === "keypress") {
+    // Enter click handler
+    if (e.which == 13 || e.keyCode == 13) {
+      localStorage.setItem("focus", e.target.innerText);
+      focus.blur();
+    }
+  } else {
+    // Blur event handler
+    localStorage.setItem("focus", e.target.innerText);
+  }
+}
+
+// Get focus function
+function getFocus() {
+  if (localStorage.getItem("focus") === null) {
+    focus.textContent = "[Enter Focus]";
+  } else {
+    focus.textContent = localStorage.getItem("focus");
+  }
+}
+
+name.addEventListener("keypress", setName);
+name.addEventListener("blur", setName);
+focus.addEventListener("keypress", setFocus);
+focus.addEventListener("blur", setFocus);
+
+// Run functions (they're asynchronous)
 showTime();
 setBgGreet();
+getName();
+getFocus();
